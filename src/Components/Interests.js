3014a49b554b116d88aab8a8b67007c30e0c63cc
pavan3008 <pavansai3008@gmail.com>
2023-03-13 17:19:51 -0700

    Added Interests

--- src/Components/Interests.js
+++ src/Components/Interests.js
@@ -13,7 +13,7 @@ class Interests extends Component {
     return (
       <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="interests">
         <div className="w-100">
-        <h2 className="mb-5">Volunteer Experience</h2>
+        <h2 className="mb-5">Interests</h2>
           {
             this.interests.map((exp, index) => (
               <div key={index} className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
